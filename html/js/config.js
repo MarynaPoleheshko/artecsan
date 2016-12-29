1cc86d90eac37fb7b834bdd25cc61898f6a966ac
maryna.p <poleheshkom@digicode.net>
2016-12-29 14:38:47 +0200

    html

--- html/js/config.js
+++ html/js/config.js
@@ -232,7 +232,21 @@ function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, IdlePro
         .state('food.inventoryUsage', {
             url: "/inventory_usage",
             templateUrl: "views/inventoryUsage.html",
-            data: { pageTitle: 'Inventory Usage'}
+            data: { pageTitle: 'Inventory Usage'},
+            resolve: {
+                loadPlugin: function ($ocLazyLoad) {
+                    return $ocLazyLoad.load([
+                        {
+                            files: ['js/plugins/moment/moment.min.js']
+                        },
+                        {
+                            name: 'datePicker',
+                            files: ['css/plugins/datapicker/angular-datapicker.css','js/plugins/datapicker/angular-datepicker.js']
+                        }
+
+                    ]);
+                }
+            }
         })
         .state('food.inventoryPurchases', {
             url: "/inventory_purchases",
